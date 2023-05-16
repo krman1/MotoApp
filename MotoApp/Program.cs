a4687aa6a09babdcb1c87d2b1eb39861cd387c8a
krman1 <mar.aniol@interia.pl>
2023-05-16 22:07:24 +0200

    stack

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var stack = new Stack<double>();
+stack.Push(30);
+stack.Push(45);
+stack.Push(26);
+stack.Push(32);
+
+double sum = 0;
+while(stack.Count > 0)
+{
+    Console.WriteLine(stack.Peek());
+
+    sum += stack.Pop();
+}
+
+Console.WriteLine("Suma wyrazów: " + sum);
