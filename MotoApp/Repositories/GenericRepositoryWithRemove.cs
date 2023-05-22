b819f8732c67aaa2cc8dd6cdd244445bfe397768
krman1 <mar.aniol@interia.pl>
2023-05-22 23:25:26 +0200

    remove

---
+++ MotoApp/Repositories/GenericRepositoryWithRemove.cs
@@ -0,0 +1,12 @@
+﻿using MotoApp.Entities;
+
+namespace MotoApp.Repositories
+{
+    public class GenericRepositoryWithRemove<T> : GenericRepository<T> where T : IEntity
+    {
+        public void Remove(T item) 
+        { 
+            _items.Remove(item);
+        }
+    }
+}
--- MotoApp/Repositories/GenericRpository.cs
+++ MotoApp/Repositories/GenericRpository.cs
@@ -2,25 +2,26 @@
 
 namespace MotoApp.Repositories
 {
-    public class Employeerepository<T> where T : IEntity
+    public class GenericRepository<T> where T : IEntity
     {
-        private readonly List<T> _items = new();
+        protected readonly List<T> _items = new();
 
         public void Add (T item)
         {
             item.Id = _items.Count+1;
             _items.Add (item);
         }
-        public void Save()
-            {
-                foreach (var item in _items)
-                {
-                    Console.WriteLine(item);
-                } 
-            }
+        
         public T GetById(int id)
         {
             return _items.Single(item => item.Id == id);
+        }
+        public void Save()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
