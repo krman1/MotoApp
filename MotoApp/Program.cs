3cf4e2235833217058eb1a4275fa9e50ee09deeb
krman1 <mar.aniol@interia.pl>
2023-05-24 15:38:23 +0200

    limitations

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using MotoApp.Repositories;
 using MotoApp.Entities;
 
-var employseeRepository = new Employeerepository();
+var employseeRepository = new GenericRepository<Employee, int>();
 employseeRepository.Add(new Employee { FirstName = "Adam" });
 employseeRepository.Add(new Employee { FirstName = "Piotr" });
 employseeRepository.Add(new Employee { FirstName = "Zuzia" });
--- MotoApp/Repositories/GenericRpository.cs
+++ MotoApp/Repositories/GenericRpository.cs
@@ -2,17 +2,21 @@
 
 namespace MotoApp.Repositories
 {
-    public class GenericRepository<T> where T : IEntity
+    public class GenericRepository<TEntity, TKey> 
+        where TEntity : class, IEntity,new()
+        where TKey : struct
     {
-        protected readonly List<T> _items = new();
+        public TKey key { get; set; }
 
-        public void Add (T item)
+        protected readonly List<TEntity> _items = new();
+
+        public void Add (TEntity item)
         {
             item.Id = _items.Count+1;
             _items.Add (item);
         }
         
-        public T GetById(int id)
+        public TEntity GetById(int id)
         {
             return _items.Single(item => item.Id == id);
         }
