ab3ea6419de5f44a84c47ac0c9392c6756f78ede
krman1 <mar.aniol@interia.pl>
2023-06-01 22:39:29 +0200

    SQL Repository

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,8 +1,17 @@
 ﻿using MotoApp.Repositories;
 using MotoApp.Entities;
+using MotoApp.Data;
 
-var employseeRepository = new GenericRepository<Employee>();
+/*var employseeRepository = new GenericRepository<Employee>();
 employseeRepository.Add(new Employee { FirstName = "Adam" });
 employseeRepository.Add(new Employee { FirstName = "Piotr" });
 employseeRepository.Add(new Employee { FirstName = "Zuzia" });
-employseeRepository.Save();
+employseeRepository.Save();*/
+
+var sqlRepository = new SqlRepository(new MotoAppDbContext());
+sqlRepository.Add(new Employee { FirstName = "Adam" });
+sqlRepository.Add(new Employee { FirstName = "Piotr" });
+sqlRepository.Add(new Employee { FirstName = "Zuzia" });
+sqlRepository.Save();
+var emp = sqlRepository.GetById(1);
+Console.WriteLine(emp.ToString());
