b92ad6af769b234a56c4737771cc4700ce35d7d9
krman1 <mar.aniol@interia.pl>
2024-01-25 23:10:36 +0100

    25.01

--- MotoApp/App.cs
+++ MotoApp/App.cs
@@ -1,10 +1,38 @@
-﻿namespace MotoApp
+﻿using MotoApp.Entities;
+using MotoApp.Repositories;
+
+namespace MotoApp
 {
     public class App : IApp
     {
+        private readonly IRepository<Employee> _employeeRepository;
+
+        public App(IRepository<Employee> employeeRepository) 
+        {
+            _employeeRepository = employeeRepository;
+        }
         public void Run()
         {
             Console.WriteLine("I'm here in Run() method");
+
+            var employees = new[]
+        {
+            new Employee { FirstName = "Marcin" },
+            new Employee { FirstName = "Iwona" },
+            new Employee { FirstName = "Mariusz" }
+        };
+            foreach (var employee in employees)
+            {
+                _employeeRepository.Add(employee);
+            }
+            _employeeRepository.Save();
+
+
+            var items = _employeeRepository.GetAll();
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,9 +1,12 @@
 ﻿
 using Microsoft.Extensions.DependencyInjection;
 using MotoApp;
+using MotoApp.Entities;
+using MotoApp.Repositories;
 
 var services = new ServiceCollection();
 services.AddSingleton<IApp, App>();
+services.AddSingleton<IRepository<Employee>, ListRepository<Employee>>();
 
 var serviceProvider = services.BuildServiceProvider();
 var app = serviceProvider.GetService<IApp>();
