cc25ca757367bbf745d73d6643e0bf661bd7f17e
krman1 <mar.aniol@interia.pl>
2024-02-02 22:13:31 +0100

    02.02

---
+++ MotoApp/Entities/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace MotoApp.Entities
+{
+    public class Car : EntitesBase
+    {
+        public string Name {  get; set; }
+        public string Color { get; set; }
+        public decimal StndardCost { get; set; }
+        public decimal ListPrice { get; set; }
+        public string Type { get; set; }    
+
+        //Calculated propertis 
+
+        public int? NameLenght { get; set; }
+        public decimal TotalSales { get; set; }
+    }
+}
