a03c44d5b2c8f44bfc08586f20e59b5b0c73fa99
krman1 <mar.aniol@interia.pl>
2024-02-13 23:37:05 +0100

    13.02

--- MotoApp/DataProviders/ICarsProvider.cs
+++ MotoApp/DataProviders/ICarsProvider.cs
@@ -1,7 +1,12 @@
 ﻿
+using MotoApp.Entities;
+
 namespace MotoApp.DataProviders
 {
     public interface ICarsProvider
     {
+        List<Car> FilterCars(decimal miniPrice);
+        List<string> GetUniqueCarColors();
+        decimal GetMinimumPriceOfAllCars();
     }
 }
