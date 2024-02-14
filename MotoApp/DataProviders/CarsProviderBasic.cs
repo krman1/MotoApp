2ff6fb475ba4e9d1687e5925dbd2e4757dc54bdb
krman1 <mar.aniol@interia.pl>
2024-02-14 22:52:08 +0100

    14.02

--- MotoApp/DataProviders/CarsProviderBasic.cs
+++ MotoApp/DataProviders/CarsProviderBasic.cs
@@ -1,7 +1,49 @@
 ﻿
+using MotoApp.Entities;
+using MotoApp.Repositories;
+
 namespace MotoApp.DataProviders
 {
-    internal class CarsProviderBasic
+    internal class CarsProviderBasic : ICarsProvider
     {
+        private readonly IRepository<Car> _carsRepository;
+        public CarsProviderBasic(IRepository<Car> carsRepository) 
+        { 
+            _carsRepository = carsRepository;
+        }
+        public List<Car> FilterCars(decimal miniPrice)
+        {
+            var cars = _carsRepository.GetAll();
+            var list = new List<Car>();
+
+            foreach (var car in cars)
+            {
+                if (car.ListPrice > miniPrice)
+                {
+                    list.Add(car);
+                }
+            }
+            return list;
+        }
+
+        public List<string> GetUniqueCarColors()
+        {
+            var cars = _carsRepository.GetAll();
+            List<string> list = new();
+
+            foreach (var car in cars)
+            {
+                if (!list.Contains(car.Color))
+                {
+                    list.Add(car.Color);
+                }
+            }
+            return list;
+        }
+
+        public decimal GetMinimumPriceOfAllCars()
+        {
+
+        }
     }
 }
